10fa09eb820398c0a5236c016fe64ac4346abeae
Max Justicz <max@justi.cz>
2017-10-29 07:17:47 -0700

    fix whitespace

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,18 @@ static LEGACY_RECORD_VERSION: u16 = 0x0301;
 static LEGACY_PROTOCOL_VERSION: u16 = 0x0303;
 
 enum HandshakeType {
-	ClientHello = 1,
-	ServerHello = 2,
-	NewSessionTicket = 4,
-	EndOfEarlyData = 5,
-	HelloRetryRequest = 6,
-	EncryptedExtensions = 8,
-	Certificate = 11,
-	CertificateRequest = 13,
-	CertificateVerify = 15,
-	Finished = 20,
-	KeyUpdate = 24,
-	MessageHash = 254,
+    ClientHello = 1,
+    ServerHello = 2,
+    NewSessionTicket = 4,
+    EndOfEarlyData = 5,
+    HelloRetryRequest = 6,
+    EncryptedExtensions = 8,
+    Certificate = 11,
+    CertificateRequest = 13,
+    CertificateVerify = 15,
+    Finished = 20,
+    KeyUpdate = 24,
+    MessageHash = 254,
 }
 
 enum ContentType {
@@ -38,13 +38,13 @@ enum ContentType {
 #[allow(non_camel_case_types)]
 #[derive(Copy, Clone)]
 enum CipherSuite {
-	TLS_RSA_WITH_AES_256_CBC_SHA = 0x0035,
-	TLS_RSA_WITH_AES_256_CBC_SHA256 = 0x003D,
+    TLS_RSA_WITH_AES_256_CBC_SHA = 0x0035,
+    TLS_RSA_WITH_AES_256_CBC_SHA256 = 0x003D,
 }
 
 #[derive(Copy, Clone)]
 enum CompressionMethod {
-	CompressionMethodNULL = 0x00,
+    CompressionMethodNULL = 0x00,
 }
 
 /*
@@ -77,14 +77,14 @@ struct CipherSuites {
 
 struct ClientHello {
     /*  struct {
-     *  	ProtocolVersion legacy_version = 0x0303;    /* TLS v1.2 */
-     *  	Random random;
-     * 		opaque legacy_session_id<0..32>;
-     *  	CipherSuite cipher_suites<2..2^16-2>;
-     *  	opaque legacy_compression_methods<1..2^8-1>;
-     *  	Extension extensions<8..2^16-1>;
-   	 *  } ClientHello;
-	 */
+     *      ProtocolVersion legacy_version = 0x0303;    /* TLS v1.2 */
+     *      Random random;
+     *      opaque legacy_session_id<0..32>;
+     *      CipherSuite cipher_suites<2..2^16-2>;
+     *      opaque legacy_compression_methods<1..2^8-1>;
+     *      Extension extensions<8..2^16-1>;
+     *  } ClientHello;
+     */
     legacy_protocol_version: u16,
     random: [u8; 32],
     legacy_session_id: SessionID,
@@ -108,74 +108,74 @@ struct TLSPlaintext {
 
 
 fn serialize_client_hello(client_hello: &ClientHello, out: &mut Vec<u8>) {
-	/*
+    /*
      * legacy_protocol_version
      */
     let _ = out.write_u16::<BigEndian>(client_hello.legacy_protocol_version);
 
-	/*
+    /*
      * random
      */
     out.extend_from_slice(&client_hello.random);
 
-	/* 
+    /* 
      * legacy_session_id
      */
-	let data = &client_hello.legacy_session_id.data;
-	assert!(data.len() < 256);
-	let _ = out.write_u8(data.len() as u8);
-	out.extend_from_slice(data);
+    let data = &client_hello.legacy_session_id.data;
+    assert!(data.len() < 256);
+    let _ = out.write_u8(data.len() as u8);
+    out.extend_from_slice(data);
 
-	/* 
+    /* 
      * cipher_suites
      */
-	let data = &client_hello.cipher_suites.data;
-	assert!((data.len()*2) < 65536);
-	let _ = out.write_u16::<BigEndian>((data.len()*2) as u16);
+    let data = &client_hello.cipher_suites.data;
+    assert!((data.len()*2) < 65536);
+    let _ = out.write_u16::<BigEndian>((data.len()*2) as u16);
 
-	// Copy each cipher suite over
-	for i in 0..data.len() {
-		let _ = out.write_u16::<BigEndian>(data[i] as u16);
-	}
+    // Copy each cipher suite over
+    for i in 0..data.len() {
+        let _ = out.write_u16::<BigEndian>(data[i] as u16);
+    }
 
     /*
      * legacy_compression_methods
      */
-	let data = &client_hello.legacy_compression_methods.data;
-	assert!(data.len() < 256);
-	let _ = out.write_u8(data.len() as u8);
+    let data = &client_hello.legacy_compression_methods.data;
+    assert!(data.len() < 256);
+    let _ = out.write_u8(data.len() as u8);
 
-	// Copy each compression method over (noting that anything other than the
+    // Copy each compression method over (noting that anything other than the
     // null compression method is nonstandard)
-	for i in 0..data.len() {
-		let _ = out.write_u8(data[i] as u8);
-	}
+    for i in 0..data.len() {
+        let _ = out.write_u8(data[i] as u8);
+    }
 
     /*
      * extensions
      */
-	let data = &client_hello.extensions.data;
-	assert!(data.len() < 65536);
-	let _ = out.write_u16::<BigEndian>(data.len() as u16);
+    let data = &client_hello.extensions.data;
+    assert!(data.len() < 65536);
+    let _ = out.write_u16::<BigEndian>(data.len() as u16);
 
-	for i in 0..data.len() {
-		let _ = out.write_u8(data[i] as u8);
-	}
+    for i in 0..data.len() {
+        let _ = out.write_u8(data[i] as u8);
+    }
 }
 
 fn wrap_in_handshake(hs_type: HandshakeType, data: &Vec<u8>, out: &mut Vec<u8>) {
-	let _ = out.write_u8(hs_type as u8);
+    let _ = out.write_u8(hs_type as u8);
     assert!(data.len() < 16777216);
-	let _ = out.write_u24::<BigEndian>(data.len() as u32);
-	out.extend_from_slice(data);
+    let _ = out.write_u24::<BigEndian>(data.len() as u32);
+    out.extend_from_slice(data);
 }
 
 fn wrap_in_tls_plaintext(c_type: ContentType, data: &Vec<u8>, out: &mut Vec<u8>) {
-	let _ = out.write_u8(c_type as u8);
-	let _ = out.write_u16::<BigEndian>(LEGACY_RECORD_VERSION);
+    let _ = out.write_u8(c_type as u8);
+    let _ = out.write_u16::<BigEndian>(LEGACY_RECORD_VERSION);
     assert!(data.len() < 65536);
-	let _ = out.write_u16::<BigEndian>(data.len() as u16);
-	out.extend_from_slice(data);
+    let _ = out.write_u16::<BigEndian>(data.len() as u16);
+    out.extend_from_slice(data);
 }
 
 fn main() {
